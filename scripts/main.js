a0eccbb172a82d58c7ecfb6d5acdab4db249fc96
Laura-Lepage <lauralepage02@hotmail.com>
2024-03-11 15:55:31 +0100

    correction + bonus

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
-//récupération des valeurs input et les mettre dans une liste d'objets
+// =============
+//   variables 
+// =============
+const original = document.querySelector('#original')
+const traduction = document.querySelector('#traduction')
 const sendButton = document.querySelector(".send")
-// la variable let mots est déclarée en dehors de l'event click et conserve son état entre les clicks = objets se rajoutent au même tableau
-let mots = JSON.parse(localStorage.getItem('mots')) || [] //les deux barres renvoient un tableau vide comme valeur par défaut ce qui permettra au code de fonctionner correctement même si aucune donnée valide n'est trouvée dans le localStorage  
 const testButton = document.querySelector(".test")
 let hiddenPart = document.querySelector(".hiddenpart")
 const backButton = document.querySelector(".back")
@@ -9,28 +11,34 @@ const compareButton = document.querySelector(".compare")
 let toTranslate = document.querySelector(".wordToTranslate")
 let finalDiv = document.querySelector(".final")
 let wordProposition = document.querySelector("#proposition")
+let correctAnswersDiv = document.querySelector(".correctAnswers")
+let ulWordsList = document.querySelector(".ulwordslist")
+let pWordsList = document.querySelector(".pwordslist")
+
+// la variable let mots est déclarée en dehors de l'event click et conserve son état entre les clicks = objets se rajoutent au même tableau
+let mots = []
 let magicNumba
 let correctAnswersCount = 0
-let correctAnswersDiv = document.querySelector(".correctAnswers")
 
-// fonction pour mettre à jour l'affichage du nombre de mots enregistrés
+// ===============
+//    fonctions
+// ===============
+
+//mettre à jour l'affichage du nombre de mots enregistrés
 function updateTotalWords(){
     const totalWords = document.querySelector(".totalWords")
     totalWords.textContent = `Il y a ${mots.length} mot(s) enregistré(s)`;
 }
 
-function getRandom() {
-    let randomIndex = Math.floor(Math.random() * mots.length)
-    return randomIndex
+// va chercher un nombre aléatoire en fonction de la longueur du tableau 
+function getRandom(table){
+    return Math.floor(Math.random() * table.length)
 }
 
-function updateLocalStorage(){
-    localStorage.setItem('mots', JSON.stringify(mots))
-}
-
+//vérifie si localStorage est déjà plein ou pas
 function loadWordsFromLocalStorage(){
-    mots = JSON.parse(localStorage.getItem('mots')) || [];
-    updateTotalWords();
+    mots = JSON.parse(localStorage.getItem('mots')) || []
+    updateTotalWords()
     if (mots.length > 0) {
         testButton.style.display = 'block';
     } else {
@@ -38,21 +46,68 @@ function loadWordsFromLocalStorage(){
     }
 }
 
+function Checklist(){
+    if (ulWordsList.childElementCount != 0){
+        pWordsList.innerHTML = ``
+    } else {
+        pWordsList.innerHTML = `Vous n'avez pas enregistré de mot(s)` 
+    }
+}
+
+
+// ============
+//    events
+// ============
+
+//vérification au lancement de la page
+loadWordsFromLocalStorage()
+
+
+
+//Quand on clique pour Enregistrer un mot
 sendButton.addEventListener('click', function(){
-    const inputBoxes = document.querySelectorAll('.inputBox')
-    
-    inputBoxes.forEach(function(inputBox) {
-        const original = inputBox.querySelector('#original')
-        const traduction = inputBox.querySelector('#traduction')
+    if (original.value.trim() !== '' && traduction.value.trim() !== ''){
+        mots.push(
+            {
+                "original" : original.value,
+                "traduction" : traduction.value,
+            }
+        )
+        //effacer la liste avant d'ajouter des nouveaux éléments
+        ulWordsList.innerHTML = "" 
 
-        if (original.value.trim() !== '' && traduction.value.trim() !== '') {
-            mots.push({ original: original.value.toLowerCase(), traduction: traduction.value.toLowerCase() })
+        for (let i = 0; i < mots.length; i++) {
+            ulWordsList.innerHTML += `
+                <li>${mots[i].original}: ${mots[i].traduction}<button class="cross">❌</button></li>
+            `
         }
+        
+        let boutons = document.querySelectorAll(".cross")
+        boutons.forEach(function(bouton){
+            bouton.addEventListener('click', function(){
+                // Récupérer l'index de l'élément à supprimer à partir de l'attribut data-index
+                let index = parseInt(bouton.getAttribute('data-index'));
+                // Supprimer l'élément du tableau mots
+                mots.splice(index, 1)
+                // Supprimer l'élément de la liste HTML
+                bouton.parentElement.remove()
+                
+                // Mettre à jour la checklist
+                Checklist()
+                updateTotalWords()
+                console.log(mots);
+                
+                
+            })     
+        })   
+        Checklist()
 
-        original.value = ''
-        traduction.value = ''
-    })
 
+    }
+    original.value = ''
+    traduction.value = ''
+    localStorage.setItem('mots', JSON.stringify(mots))
+    
     console.log(mots);
 
     //affichage de la phrase avec le total de mot(s) enregistré(s)
@@ -65,28 +120,32 @@ sendButton.addEventListener('click', function(){
         testButton.style.display = 'none';
     }
 
-    updateLocalStorage()
+    
+
+    
 })
 
 
-//effacer la liste en cours
+
+
+//Quand on clique sur le bouton Effacer = effacer la liste en cours
 const clearButton = document.querySelector(".clearList")
 
 clearButton.addEventListener('click', function(){
     mots = []
-    localStorage.removeItem('mots')
+    localStorage.clear()
     updateTotalWords()
     testButton.style.display = 'none'
+
+    ulWordsList.innerHTML = ""
+    pWordsList.innerHTML = `Vous n'avez pas enregistré de mot(s)` 
    
 })
-
-loadWordsFromLocalStorage()
-
 
 console.log(mots);
 
 testButton.addEventListener('click', function(){
-    magicNumba = getRandom()
+    magicNumba = getRandom(mots)
     hiddenPart.classList.add("slide")
     toTranslate.innerHTML = 
     `
@@ -97,6 +156,8 @@ testButton.addEventListener('click', function(){
 
 backButton.addEventListener('click', function(){
     hiddenPart.classList.remove("slide")
+    correctAnswersDiv.textContent = ""
+    correctAnswersCount = 0
 })
 
 compareButton.addEventListener('click', function(){
@@ -104,7 +165,7 @@ compareButton.addEventListener('click', function(){
     if (wordProposition.value == mots[magicNumba].traduction){ 
         finalDiv.textContent = `Good 😄!`
         correctAnswersCount++;
-        magicNumba = getRandom()
+        magicNumba = getRandom(mots)
         toTranslate.innerHTML = 
         `
         <h2>Traduis le mot <span>${mots[magicNumba].original}</span></h2> `
